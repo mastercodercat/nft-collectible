3668254af1d3edaf81c383f3f44be051c15c6611
mastercodercat <smartgeek.taylor@gmail.com>
2022-04-08 02:40:26 -0700

    Initialized repository

---
+++ contracts/NFTCollectible.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
